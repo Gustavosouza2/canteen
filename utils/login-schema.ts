2f9a09479b82b9842d3718ce75879ac4ddd0659a
GustavoSouza2 <gustavoleonsouza@gmail.com>
2024-09-04 22:57:19 -0300

    chore: create new emailRegex to login

--- utils/login-schema.ts
+++ utils/login-schema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod'
 
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'**-/=?^_`{|}~]{1,64}@([a-zA-Z0-9-]{1,63}\.){1,}[a-zA-Z]{2,}$/
+
 export const LoginSchema = z.object({
-  email: z.string().email({ message: 'Invalid Email Address' }),
+  email: z
+    .string()
+    .regex(emailRegex, { message: 'Please enter a valid email' }),
   password: z.string().min(8, { message: 'Password is too short' }),
 })
