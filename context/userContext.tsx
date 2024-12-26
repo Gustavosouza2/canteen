3c4dfe5d4d5abae217e1d9ea9534eb87be60f158
GustavoSouza2 <gustavoleonsouza@gmail.com>
2024-12-26 19:09:38 -0300

    feat:create user context

---
+++ context/userContext.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { useCookies } from 'react-cookie'
+import {
+  createClientComponentClient,
+  Session,
+  User,
+} from '@supabase/auth-helpers-nextjs'
+import {
+  createContext,
+  useContext,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react'
+
+type UserType = {
+  session: Session | null
+  token: string | null
+  user: User | null
+}
+
+const UserContext = createContext<UserType | undefined>({
+  session: null,
+  token: null,
+  user: null,
+})
+
+export const UserContextProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [session, setSession] = useState<Session | null>(null)
+  const [user, setUser] = useState<User | null>(null)
+  const [cookies, setCookie] = useCookies(['token'])
+  const supabase = createClientComponentClient()
+
+  useEffect(() => {
+    const getUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+      const session = await supabase.auth.getSession()
+
+      setSession(session.data.session)
+      setUser(user)
+    }
+
+    getUser()
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        setUser(session?.user ?? null)
+        setCookie('token', session?.access_token, { path: '/' })
+        setSession(session)
+      } else if (event === 'SIGNED_OUT') {
+        setUser(null)
+        setSession(null)
+      }
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [supabase, setUser, setCookie])
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        session,
+        token: cookies?.token,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserContextProvider')
+  }
+  return {
+    userName: context?.user?.id ?? null,
+    token: context?.token ?? null,
+    email: context.user?.email ?? null,
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,12 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
+  const token = req.cookies?.get('token')
+
+  if (!token && req.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect('/login')
+  }
+
   const res = NextResponse.next()
 
   const supabase = createMiddlewareClient({ req, res })
