686bdca759cc98df1b1b179e883a63986ebf2af2
GustavoSouza2 <gustavoleonsouza@gmail.com>
2024-12-26 19:07:11 -0300

    chore:config dashboard and root layout files

--- app/(dashboard)/dashboard/home/page.tsx
+++ app/(dashboard)/dashboard/home/page.tsx
@@ -1,60 +1,24 @@
-import { DataTable } from '@/components/Table'
-import { Button } from '@/components/ui/button'
+'use client'
+
+import { useUser } from '../../../../context/userContext'
+import { Card } from '@/components/Card/Card'
 
 export default function Home() {
-  const columns = [
-    { name: 'testando table', label: 'Testeeee', size: '100%' },
-  ] as const
+  const { email, token, userName } = useUser()
 
-  const invoices = [
-    {
-      invoice: 'INV001',
-      paymentStatus: 'Paid',
-      totalAmount: '$250.00',
-      paymentMethod: 'Credit Card',
-    },
-    {
-      invoice: 'INV002',
-      paymentStatus: 'Pending',
-      totalAmount: '$150.00',
-      paymentMethod: 'PayPal',
-    },
-    {
-      invoice: 'INV003',
-      paymentStatus: 'Unpaid',
-      totalAmount: '$350.00',
-      paymentMethod: 'Bank Transfer',
-    },
-    {
-      invoice: 'INV004',
-      paymentStatus: 'Paid',
-      totalAmount: '$450.00',
-      paymentMethod: 'Credit Card',
-    },
-    {
-      invoice: 'INV005',
-      paymentStatus: 'Paid',
-      totalAmount: '$550.00',
-      paymentMethod: 'PayPal',
-    },
-    {
-      invoice: 'INV006',
-      paymentStatus: 'Pending',
-      totalAmount: '$200.00',
-      paymentMethod: 'Bank Transfer',
-    },
-    {
-      invoice: 'INV007',
-      paymentStatus: 'Unpaid',
-      totalAmount: '$300.00',
-      paymentMethod: 'Credit Card',
-    },
-  ]
+  console.log({
+    email,
+    token,
+    userName,
+  })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DataTable columns={columns} data={invoices} />
-      <Button>DSADADSA</Button>
+    <main className="flex flex-col items-center justify-between">
+      <Card
+        title="subscription"
+        description="total de valores gastos esse mes"
+        info="+360"
+      />
     </main>
   )
 }
