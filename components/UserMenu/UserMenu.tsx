1cac383364aaa8dc1ca1049c56b4ed7b9917b9d2
GustavoSouza2 <gustavoleonsouza@gmail.com>
2024-12-27 17:40:36 -0300

    feat:create user menu component and dashboard header

---
+++ components/UserMenu/UserMenu.tsx
@@ -0,0 +1,47 @@
+import { ChevronDown } from 'lucide-react'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '../ui/button'
+
+interface UserMenuProps {
+  name: string | null
+  email: string | null
+}
+export const UserMenu = ({ email, name }: UserMenuProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger
+        asChild
+        className="md:mr-16 bg-transparent border-1 border-[#a1a1aa28] hover:bg-[#1E1E20] hover:text-[#373738]"
+      >
+        <Button variant="outline" size="icon">
+          <ChevronDown className="text-[#fafafaaf] hover:bg-[#1E1E20] hover:text-[#373738]" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex border-[#fafafa70] bg-[#1E1E20] mr-10 mt-2">
+        <div className=" flex-col gap-2 justify-end items-center">
+          <div className="flex flex-row gap-2">
+            <p className="text-[#fafafaaf] text-sm font-mono font-semibold">
+              Name:
+            </p>
+            <p className="text-[#fafafaaf] text-sm font-mono font-light">
+              {name}
+            </p>
+          </div>
+
+          <div className="flex flex-row gap-2">
+            <p className="text-[#fafafaaf] text-sm font-mono font-semibold">
+              Email:
+            </p>
+            <p className="text-[#fafafaaf] text-sm font-mono font-light">
+              {email}
+            </p>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
---
+++ components/layout/dashboard/Header.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { UserMenu } from '@/components/UserMenu/UserMenu'
+
+import { useUser } from '@/context/userContext'
+
+export const HeaderDashboard = () => {
+  const { email } = useUser()
+
+  const rawUserName = email?.slice(0, 7)
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-10 flex justify-end items-center h-16 px-4 bg-[#1E1E20] border-b-2 border-[#a1a1aa28]">
+      <div className="flex flex-row gap-2">
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+            height={15}
+            width={15}
+          />
+          <AvatarFallback>GS</AvatarFallback>
+        </Avatar>
+        <UserMenu
+          name={rawUserName || 'Admin'}
+          email={email || 'Admin@gmail.com'}
+        />
+      </div>
+    </header>
+  )
+}
