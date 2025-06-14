3dd17f3f0861825c97a1f9b0a93fd820efddf363
Gustavo Souza <gustavoleonsouza@gmail.com>
2025-06-13 21:58:27 -0300

    build:eslint build error

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,5 @@
-import { cookies } from 'next/headers'
 import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
 export async function createSupabaseServerClient() {
   const cookieStore = await cookies()
